6a04ad1ca51b3754368788b99e450952a2f9245f
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-14 19:13:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header>
+        <Navbar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  )
+}
+
+export default App
