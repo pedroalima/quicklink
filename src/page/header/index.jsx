23004b1bdaa4cacc5c38df5d1324609d595bcbb0
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-16 17:37:52 -0300

    style: add mobile menu

--- src/page/header/index.jsx
+++ src/page/header/index.jsx
@@ -7,7 +7,64 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 function Header({ typeDisplay }) {
     return (
         <header className="container">
-            {!typeDisplay ? (
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Logo" />
+                </a>
+                <button 
+                    className="navbar-toggler border-0" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                    >
+                    <FontAwesomeIcon icon={faBars} size='xl' />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    {!typeDisplay ? (
+                    <>
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link text-light" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-light" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-light" href="#">Resources</a>
+                            </li>
+                        </ul>
+                        <hr />
+                        <form className="form d-flex flex-column">
+                            <button className="btn text-light rounded-pill" type="button">Login</button>
+                            <button className="btn text-light rounded-pill btn-costumer" type="button">Sing Up</button>
+                        </form>
+                    </>
+                    ) : (
+                    <>
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link text-secondary" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-secondary" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-secondary" href="#">Resources</a>
+                            </li>
+                        </ul>
+                        <form className="form d-flex flex-row">
+                            <button className="btn text-secondary rounded-pill" type="button">Login</button>
+                            <button className="btn text-light rounded-pill btn-costumer" type="button">Sing Up</button>
+                        </form>
+                    </>
+                    )}
+                    
+                </div>
+            </nav>
+            {/* {!typeDisplay ? (
                     <nav className="row align-items-center">
                         <div className="col-10">
                             <h1><img src={logo} alt="Logo" /></h1>
@@ -34,7 +91,7 @@ function Header({ typeDisplay }) {
                         </div>
                     </nav>
                 )
-            }
+            } */}
         </header>
     )
 }
